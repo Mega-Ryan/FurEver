1455e9e068b71e738e3682ab4a50f158010f146c
Ryan Zenn <ryan_zenn@outlook.com>
2023-03-01 10:49:00 +0800

    111

--- miniprogram/pages/cloudAdoption/cloudAdoption.js
+++ miniprogram/pages/cloudAdoption/cloudAdoption.js
@@ -1,16 +1,17 @@
 // pages/cloudAdoption/cloudAdoption.js
 const db = wx.cloud.database()
+
 const app = getApp()
 
 
 Page({
+
   /**
    * 页面的初始数据
    */
   data: {
-    animal: []
+    animal:[]
   },
-  
   //路由传参到动物详情页
   toDetail(e){
     console.log(e)
@@ -18,23 +19,24 @@ Page({
       url: `/pages/detail/detail?id=${e.currentTarget.id}`,
     })
   },
-  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
     var that = this
-    db.collection('test').where({}).get({
-      success: function(res) {
+    db.collection('test').where({
+      isApproval:true
+    }).get({
+      success: res => {
         // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
         console.log(res)
         that.setData({
-          animal: res.data,
+          animal:res.data,
         })
         console.log(that.data.animal)
       }
     })
-
+    
   },
 
   /**
@@ -48,12 +50,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    console.log("onShow")
-    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-      this.getTabBar().setData({
-        selected: 1
-      })
-    }
+
   },
 
   /**
